ed019f08778bfcead42901d189d0fdc389e7aee0
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-09-21 03:21:34 +0100

    fixed: some betty styling

--- functions0.c
+++ functions0.c
@@ -10,22 +10,22 @@ void push(monty_stack_t **stack, unsigned int line_number)
 {
 	monty_stack_t *new_node;
 	extern int argument_value;
-	
+
 	(void)line_number;
 
-    new_node = malloc(sizeof(monty_stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(monty_stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = argument_value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	new_node->n = argument_value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
 
 /**
--- functions1.c
+++ functions1.c
@@ -9,14 +9,14 @@ int argument_value = 0;
  */
 void free_stack(monty_stack_t *stack)
 {
-    monty_stack_t *temp;
+	monty_stack_t *temp;
 
-    while (stack != NULL)
-    {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+	while (stack != NULL)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
 }
 
 /**
@@ -28,40 +28,40 @@ void free_stack(monty_stack_t *stack)
  */
 void execute(monty_stack_t **stack, char *token, unsigned int line_number)
 {
-    int i = 0;
-    char *opcode;
-    char *argument;
+	int i = 0;
+	char *opcode;
+	char *argument;
 
-    instruction_t opcodes[] = {
-        {"push", push},
-        {"pall", pall},
-        {NULL, NULL}
-    };
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
 
-    opcode = strtok(token, " \n\t");
-    argument = strtok(NULL, " \n\t");
+	opcode = strtok(token, " \n\t");
+	argument = strtok(NULL, " \n\t");
 
-    if (opcode && strcmp(opcode, "push") == 0)
-    {
-        if (!argument || !isdigit(argument[0]))
-        {
-            fprintf(stderr, "L%u: usage: push integer\n", line_number);
-            exit(EXIT_FAILURE);
-        }
-        argument_value = atoi(argument);
-    }
+	if (opcode && strcmp(opcode, "push") == 0)
+	{
+		if (!argument || !isdigit(argument[0]))
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		argument_value = atoi(argument);
+	}
 
-    while (opcodes[i].opcode != NULL)
-    {
-        if (strcmp(opcode, opcodes[i].opcode) == 0)
-        {
-            opcodes[i].f(stack, line_number);
-            return;
-        }
-        i++;
-    }
-    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-    exit(EXIT_FAILURE);
+	while (opcodes[i].opcode != NULL)
+	{
+		if (strcmp(opcode, opcodes[i].opcode) == 0)
+		{
+			opcodes[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
 }
 
 
